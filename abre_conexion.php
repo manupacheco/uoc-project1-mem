edbe365a29c16cec4a4d5e4d6c282e486aab123f
epereiraf <45182312+epereiraf@users.noreply.github.com>
2018-12-26 16:06:05 +0100

    Update abre_conexion.php

--- abre_conexion.php
+++ abre_conexion.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "registro";
+$username = "id7798627_grupomem";
+$password = "12345";
+$dbname = "id7798627_registro";
 
 $conexion= mysqli_connect($servername, $username, $password,$dbname);
 if(!$conexion){
